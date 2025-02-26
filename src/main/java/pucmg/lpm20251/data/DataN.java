77fa0764e9ee4f30a3fc436c9b0a5c2ca3ae727d
Danilo B S <daniloboechat@msn.com>
2025-02-25 22:08:45 -0300

    data noite -- construtores

---
+++ src/main/java/pucmg/lpm20251/data/DataN.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pucmg.lpm20251.data;
+
+/**
+ *
+ * @author danil
+ */
+public class DataN {
+
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public DataN(int dia, int mes, int ano) {
+        init(dia, mes, ano);
+    }
+
+    public DataN(int dia, int mes) {
+        init(dia, mes, 2025);
+    }
+
+    public DataN() {
+        //init(1, 1, 2005);
+    }
+    private void init(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+
+        if (!dataValida()) {
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2005;
+        }
+        
+    }
+    public void ajustarData(int dia, int mes, int ano) {
+        init(dia, mes, ano);
+    }
+
+    public boolean anoBissexto() {
+        if ((this.ano % 4 == 0 && this.ano % 100 != 0) || (this.ano % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean dataValida() {
+        int[] mes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (this.ano < 0) {
+            return false;
+        }
+
+        if (this.mes < 1 || this.mes > 12) {
+            return false;
+        }
+
+        if (anoBissexto()) {
+            mes[1] = 29;
+        }
+
+        if (this.dia < 1 && this.dia > mes[this.mes - 1]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String dataFormatada() {
+        return this.dia + "/" + this.mes + "/" + this.ano;
+    }
+
+}
---
+++ src/main/java/pucmg/lpm20251/data/MainN.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pucmg.lpm20251.data;
+
+/**
+ *
+ * @author danil
+ */
+public class MainN {
+
+    public static void main(String[] args) {
+
+        DataN data1 = new DataN(29, 2, 2025);
+        System.out.println(data1.anoBissexto());
+
+        DataN data2 = new DataN(30, 1);
+        System.out.println(data2.dataFormatada());
+
+        DataN data3 = new DataN();
+        //data1.ajustarData(29, 02, -1000);
+        //System.out.println(data1.dataFormatada());
+        //System.out.println(data1.dataValida());
+
+    }
+
+}
